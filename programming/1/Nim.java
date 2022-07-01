27b20d2e52bdc06cfe4e11734072c2787ff301e9
SAYbaw <97255560+SAYbaw@users.noreply.github.com>
2022-07-01 21:11:51 +0000

    finished basic methods

---
+++ programming/1/Nim.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public static void main (String[] args) {
+  int stones = 12;
+  int stonesTaken;
+
+  Scanner input = new Scanner(System.in)
+
+    //loop until gam ends
+
+    //prompt user imput
+
+    
+  
+}
